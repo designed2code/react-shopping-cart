d3797a58be5d9a1a57865a4a9df7c5d7919e2c96
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-01-18 23:28:18 +0530

    First Commit

---
+++ src/redux/Shopping/shopping-reducer.js
@@ -0,0 +1,83 @@
+import * as actionTypes from "./shopping-types";
+
+const INITIAL_STATE = {
+  products: [
+    {
+      id: 1,
+      title: "Men's Denim Jacket",
+      description:
+        "Add this classic denim jacket to your casual wardrobe. Made from soft and breathable cotton. Stonewashed finish gives the style that lived-in look. Finished with two breast pockets. All of the cotton for our clothing is sustainably sourced and always will be.",
+      price: 1500.0,
+      image:
+        "https://images.unsplash.com/photo-1495105787522-5334e3ffa0ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 2,
+      title: "Nike Air Jordan",
+      description:
+        "Supplied by a premier sneaker marketplace dealing with unworn, already sold out, in demand rarities. Each product is rigorously inspected by experienced experts guaranteeing authenticity. The Women’s Air Jordan 1 “Satin Black Toe” is a special construction of the original colorway of the Jordan 1 with ",
+      price: 2000.0,
+      image:
+        "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+    },
+    {
+      id: 3,
+      title: "FOSSIL Townsman",
+      description:
+        "Designed exclusively for the modern gentlemen with a penchant for luxury, this chronograph watch featuring 50m water resistance and three subdials facilitating better time reading maintains your sophisticated sensibilities at any time",
+      price: 4000.0,
+      image:
+        "https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+  ],
+  cart: [],
+  currentItem: null,
+};
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_TO_CART:
+      // Great Item data from products array
+      const item = state.products.find(
+        (product) => product.id === action.payload.id
+      );
+      // Check if Item is in cart already
+      const inCart = state.cart.find((item) =>
+        item.id === action.payload.id ? true : false
+      );
+
+      return {
+        ...state,
+        cart: inCart
+          ? state.cart.map((item) =>
+              item.id === action.payload.id
+                ? { ...item, qty: item.qty + 1 }
+                : item
+            )
+          : [...state.cart, { ...item, qty: 1 }],
+      };
+    case actionTypes.REMOVE_FROM_CART:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
+    case actionTypes.ADJUST_ITEM_QTY:
+      return {
+        ...state,
+        cart: state.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, qty: +action.payload.qty }
+            : item
+        ),
+      };
+    case actionTypes.LOAD_CURRENT_ITEM:
+      return {
+        ...state,
+        currentItem: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default shopReducer;
